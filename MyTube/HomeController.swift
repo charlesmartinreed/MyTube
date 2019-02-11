98cf93bd23ed7fb331b768cdd84663bde4463913
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-11 14:49:04 -0600

    Implement profile image, title and subtitle.

--- MyTube/HomeController.swift
+++ MyTube/HomeController.swift
@@ -53,7 +53,9 @@ class VideoCell: UICollectionViewCell {
     //each cell has a thumbnail image
     let thumbnailImageView: UIImageView = {
        let imageView = UIImageView()
-        imageView.backgroundColor = UIColor.blue
+        imageView.image = #imageLiteral(resourceName: "blank_space_image")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         
         return imageView
     }()
@@ -65,25 +67,33 @@ class VideoCell: UICollectionViewCell {
         return seperator
     }()
     
-    let profileImageView: UIView = {
-        let profile = UIView()
-        profile.backgroundColor = UIColor.green
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "profile_image")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         
-        return profile
+        imageView.layer.cornerRadius = 22 //half of width/height of frame
+        imageView.layer.masksToBounds = true
+        
+        return imageView
     }()
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = UIColor.purple
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Taylor Swift - Blank Space"
         
         return label
     }()
     
     let subtitleTextView: UITextView = {
         let textView = UITextView()
-        textView.backgroundColor = UIColor.red
         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "TaylorSwiftVEVO • 1,604,684,507 views • 2 years"
+        //Subtitle label inset fix
+        textView.textContainerInset = UIEdgeInsets(top: 0, left: -4, bottom: 0, right: 0)
+        textView.textColor = UIColor.lightGray
         
         return textView
     }()
@@ -110,10 +120,10 @@ class VideoCell: UICollectionViewCell {
         titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
         
         //subtitleTextView constraints
-        subtitleTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        subtitleTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2).isActive = true
         subtitleTextView.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 8).isActive = true
         subtitleTextView.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 0).isActive = true
-        subtitleTextView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        subtitleTextView.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
         
     }
