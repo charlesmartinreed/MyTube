6d4b81cf181ac3e674b59f3b5a28295c372377c7
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-11 17:14:38 -0600

    Create and position collectionView within menu bar.

--- MyTube/View/MenuBar.swift
+++ MyTube/View/MenuBar.swift
@@ -9,14 +9,57 @@
 import UIKit
 
 class MenuBar : UIView {
+    //collection view contains buttons and the highlight and interactable states
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31, alpha: 1)
+        
+        cv.dataSource = self
+        cv.delegate = self
+        
+        return cv
+    }()
+    
+    let cellId = "cellId"
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31, alpha: 1)    }
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        addSubview(collectionView)
+        addConstraintWithFormat(format: "H:|[v0]|", views: collectionView)
+        addConstraintWithFormat(format: "V:|[v0]|", views: collectionView)
+        }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+extension MenuBar : UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4 //because we need four buttons
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = UIColor.blue
+        
+        return cell
+    }
+}
+
+extension MenuBar : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 4, height: frame.height)
+    }
+    
+    //spacing between cells fix
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
     
 }
